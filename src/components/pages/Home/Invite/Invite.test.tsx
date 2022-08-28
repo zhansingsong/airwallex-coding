ae5ca8efdb18677c1f32001bf4f8e9fd93678c38
zhansingsong <zhansingsong@gmail.com>
2022-08-29 02:05:23 +0800

    test: add Invite test cases

--- src/components/pages/Home/Invite/Invite.test.tsx
+++ src/components/pages/Home/Invite/Invite.test.tsx
@@ -1,7 +1,23 @@
 import React from 'react';
 import { render, screen, fireEvent, waitFor } from '@testing-library/react';
+import user from '@testing-library/user-event';
 import { BrowserRouter as Router } from 'react-router-dom';
+import axios from 'axios'; // eslint-disable-line
 import Invite from './Invite';
+
+jest.mock('axios', () => {
+  return {
+    __esModule: true,
+    default: (agrs: any) => {
+      // eslint-disable-line
+      return agrs.data.email === 'usedemail@airwallex.com'
+        ? Promise.reject({
+            response: { data: { errorMessage: 'Bad Request: Email is already in use' } }, // eslint-disable-line
+          }) // eslint-disable-line
+        : Promise.resolve('Registered');
+    },
+  };
+});
 describe('Invite component', () => {
   test('renders Invite', () => {
     render(
@@ -27,7 +43,7 @@ describe('Invite component', () => {
     });
   });
 
-  test('Request an Invite Form', async () => {
+  test('[name] field validation1', async () => {
     render(
       <Router>
         <Invite />
@@ -45,24 +61,183 @@ describe('Invite component', () => {
       expect(screen.getByTestId('alertTip')).toHaveTextContent('[name] cannot be empty!');
     });
   });
-  // test('Request an Invite Form', async () => {
-  //   render(
-  //     <Router>
-  //       <Invite />
-  //     </Router>
-  //   );
-  //   const requestBtn = screen.getByRole('invite');
-  //   expect(requestBtn).toBeInTheDocument();
-  //   fireEvent.click(requestBtn);
-  //   await waitFor(() => {
-  //     const alertElement = screen.getByText('Request an invite');
-  //     expect(alertElement).toBeInTheDocument();
-  //     const submit = screen.getByTestId('submit');
-  //     expect(submit).toBeInTheDocument();
-  //     fireEvent.change(screen.getByPlaceholderText('name'), { target: { value: '2312' } });
-  //     fireEvent.submit(screen.getByTestId('alertForm'));
-  //     expect(screen.getByTestId('alertTip')).toHaveTextContent('[name] cannot be empty!');
-  //     console.log(screen.getByTestId('alertTip').innerHTML);
-  //   });
-  // });
+  test('[name] field validation2', async () => {
+    render(
+      <Router>
+        <Invite />
+      </Router>
+    );
+    const requestBtn = screen.getByRole('invite');
+    expect(requestBtn).toBeInTheDocument();
+    fireEvent.click(requestBtn);
+    await waitFor(async () => {
+      const alertElement = screen.getByText('Request an invite');
+      expect(alertElement).toBeInTheDocument();
+      const submit = screen.getByTestId('submit');
+      expect(submit).toBeInTheDocument();
+      user.type(await screen.getByPlaceholderText('name'), '12');
+      fireEvent.submit(await screen.getByTestId('alertForm'));
+      const alertTip = await screen.getByTestId('alertTip');
+      expect(alertTip).toHaveTextContent('[name] cannot be less than 3 characters long!');
+    });
+  });
+  test('[email] field validation1', async () => {
+    render(
+      <Router>
+        <Invite />
+      </Router>
+    );
+    const requestBtn = screen.getByRole('invite');
+    expect(requestBtn).toBeInTheDocument();
+    fireEvent.click(requestBtn);
+    await waitFor(async () => {
+      const alertElement = screen.getByText('Request an invite');
+      expect(alertElement).toBeInTheDocument();
+      const submit = screen.getByTestId('submit');
+      expect(submit).toBeInTheDocument();
+      user.type(screen.getByPlaceholderText('name'), '1244');
+      fireEvent.submit(screen.getByTestId('alertForm'));
+      const alertTip = await screen.getByTestId('alertTip');
+      expect(alertTip).toHaveTextContent('[email] cannot be empty!');
+    });
+  });
+  test('[email] field validation2', async () => {
+    render(
+      <Router>
+        <Invite />
+      </Router>
+    );
+    const requestBtn = screen.getByRole('invite');
+    expect(requestBtn).toBeInTheDocument();
+    fireEvent.click(requestBtn);
+    await waitFor(async () => {
+      const alertElement = screen.getByText('Request an invite');
+      expect(alertElement).toBeInTheDocument();
+      const submit = screen.getByTestId('submit');
+      expect(submit).toBeInTheDocument();
+      user.type(screen.getByPlaceholderText('name'), '1244');
+      user.type(screen.getByPlaceholderText('email'), '1244');
+      fireEvent.submit(screen.getByTestId('alertForm'));
+      const alertTip = await screen.getByTestId('alertTip');
+      expect(alertTip).toHaveTextContent('[email] is not a valid email!');
+    });
+  });
+  test('[email] field validation3', async () => {
+    render(
+      <Router>
+        <Invite />
+      </Router>
+    );
+    const requestBtn = screen.getByRole('invite');
+    expect(requestBtn).toBeInTheDocument();
+    fireEvent.click(requestBtn);
+    await waitFor(async () => {
+      const alertElement = screen.getByText('Request an invite');
+      expect(alertElement).toBeInTheDocument();
+      const submit = screen.getByTestId('submit');
+      expect(submit).toBeInTheDocument();
+      user.type(screen.getByPlaceholderText('name'), '1244');
+      user.type(screen.getByPlaceholderText('email'), '123@121');
+      fireEvent.submit(screen.getByTestId('alertForm'));
+      const alertTip = await screen.getByTestId('alertTip');
+      expect(alertTip).toHaveTextContent('[email] is not a valid email!');
+    });
+  });
+  test('[Confirm email] field validation1', async () => {
+    render(
+      <Router>
+        <Invite />
+      </Router>
+    );
+    const requestBtn = screen.getByRole('invite');
+    expect(requestBtn).toBeInTheDocument();
+    fireEvent.click(requestBtn);
+    await waitFor(async () => {
+      const alertElement = screen.getByText('Request an invite');
+      expect(alertElement).toBeInTheDocument();
+      const submit = screen.getByTestId('submit');
+      expect(submit).toBeInTheDocument();
+      user.type(screen.getByPlaceholderText('name'), '1244');
+      user.type(screen.getByPlaceholderText('email'), '123@121.com');
+      fireEvent.submit(screen.getByTestId('alertForm'));
+      await waitFor(async () => {
+        const alertTip = await screen.getByTestId('alertTip');
+        expect(alertTip).toHaveTextContent('[Confirm email] cannot be empty!');
+      });
+    });
+  });
+  test('[Confirm email] field validation2', async () => {
+    render(
+      <Router>
+        <Invite />
+      </Router>
+    );
+    const requestBtn = screen.getByRole('invite');
+    expect(requestBtn).toBeInTheDocument();
+    fireEvent.click(requestBtn);
+    await waitFor(async () => {
+      const alertElement = screen.getByText('Request an invite');
+      expect(alertElement).toBeInTheDocument();
+      const submit = screen.getByTestId('submit');
+      expect(submit).toBeInTheDocument();
+      user.type(screen.getByPlaceholderText('name'), '1244');
+      user.type(screen.getByPlaceholderText('email'), '123@121.com');
+      user.type(screen.getByPlaceholderText('Confirm email'), '123@.com');
+      fireEvent.submit(screen.getByTestId('alertForm'));
+      await waitFor(async () => {
+        const alertTip = await screen.getByTestId('alertTip');
+        expect(alertTip).toHaveTextContent('[Confirm email] confirmation is incorrect！');
+      });
+    });
+  });
+  test('form submit ok', async () => {
+    render(
+      <Router>
+        <Invite />
+      </Router>
+    );
+    const requestBtn = screen.getByRole('invite');
+    expect(requestBtn).toBeInTheDocument();
+    fireEvent.click(requestBtn);
+    await waitFor(async () => {
+      const alertElement = screen.getByText('Request an invite');
+      expect(alertElement).toBeInTheDocument();
+      const submit = screen.getByTestId('submit');
+      expect(submit).toBeInTheDocument();
+      user.type(screen.getByPlaceholderText('name'), '1244');
+      user.type(screen.getByPlaceholderText('email'), '123@121.com');
+      user.type(screen.getByPlaceholderText('Confirm email'), '123@121.com');
+      fireEvent.submit(screen.getByTestId('alertForm'));
+      const alertTip = await screen.getByText('sending! please wait');
+      expect(alertTip).toBeInTheDocument();
+      await waitFor(async () => {
+        const ok = await screen.getByText('Ok');
+        expect(ok).toBeInTheDocument();
+      });
+    });
+  });
+  test('form submit fail', async () => {
+    render(
+      <Router>
+        <Invite />
+      </Router>
+    );
+    const requestBtn = screen.getByRole('invite');
+    expect(requestBtn).toBeInTheDocument();
+    fireEvent.click(requestBtn);
+    await waitFor(async () => {
+      const alertElement = screen.getByText('Request an invite');
+      expect(alertElement).toBeInTheDocument();
+      const submit = screen.getByTestId('submit');
+      expect(submit).toBeInTheDocument();
+      user.type(screen.getByPlaceholderText('name'), '1244');
+      user.type(screen.getByPlaceholderText('email'), 'usedemail@airwallex.com');
+      user.type(screen.getByPlaceholderText('Confirm email'), 'usedemail@airwallex.com');
+      fireEvent.submit(screen.getByTestId('alertForm'));
+      await waitFor(async () => {
+        const alertTip = await screen.getByTestId('alertTip');
+        expect(alertTip).toHaveTextContent('Bad Request: Email is already in use');
+      });
+    });
+  });
 });
